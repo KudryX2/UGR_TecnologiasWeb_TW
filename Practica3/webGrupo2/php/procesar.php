a2dd4d7eb9193ce3dc99abc5fdd81ce904dcb37a
Kudry <15013415+KudryX2@users.noreply.github.com>
2019-11-28 17:05:24 +0100

    Add files via upload

---
+++ Practica3/webGrupo2/php/procesar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Variables recibidas</title>
+	</head>
+
+	<body>
+		<?php
+			echo "<p>Variables GET: </p>";
+			echo "<ul>";
+			foreach ($_GET as $c => $v) {
+				if(is_array($v)){
+					echo "<li>$c = ";
+					print_r($v);
+					echo "</li>";
+				}else{
+					echo "<li>$c = $v</li>";
+				}
+			}
+			echo "</ul>";
+
+			echo "<p>Variables POST: </p>";
+			echo "<ul>";
+			foreach ($_POST as $c => $v) {
+				if(is_array($v)){
+					echo "<li>$c = ";
+					print_r($v);
+					echo "</li>";
+				}else{
+					echo "<li>$c = $v</li>";
+				}
+			}
+			echo "</ul>";
+		?>
+	</body>
+
+</html>
